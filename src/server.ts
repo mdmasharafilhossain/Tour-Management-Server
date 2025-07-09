0d2e0c952e7262d4f24facbd4f3786ec7f62d017
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-07-09 12:46:47 +0600

    MongoDB Connnected Successfully

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import {Server} from "http";
+import express from "express";
+import mongoose from "mongoose";
+
+let  server: Server;
+
+const app = express()
+
+const startServer = async ()=>{
+    try{
+await mongoose.connect("mongodb+srv://Tour-Management-L2:7XnwneOLXHNJc9Tx@cluster0.lzichn4.mongodb.net/Tour-Management-L2?retryWrites=true&w=majority&appName=Cluster0")
+
+    console.log("Connected to DB!!");
+    server = app.listen(5000, ()=>{
+        console.log("Server is Listening to port 5000");
+    })
+    }catch(error){
+console.log(error);
+    }
+}
+
+startServer()
