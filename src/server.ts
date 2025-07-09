b0247a75724757f880b29af0f27c94877b2a5745
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-07-09 17:32:17 +0600

    env file added

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 
 import mongoose from "mongoose";
 import app from "./app";
+import { envVars } from "./app/config/env";
 
 
 
@@ -10,12 +11,12 @@ import app from "./app";
 const startServer = async ()=>{
     try{
         
-await mongoose.connect("mongodb+srv://Tour-Management-L2:7XnwneOLXHNJc9Tx@cluster0.lzichn4.mongodb.net/Tour-Management-L2?retryWrites=true&w=majority&appName=Cluster0")
+await mongoose.connect(envVars.DB_URL)
 
     console.log("Connected to DB!!");
 
-    app.listen(5000, ()=>{
-        console.log("Server is Listening to port 5000");
+    app.listen(envVars.PORT, ()=>{
+        console.log(`Server is Listening to port ${envVars.PORT}`);
     })
     }catch(error){
 console.log(error);
