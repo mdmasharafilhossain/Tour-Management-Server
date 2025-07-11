e3e13f6fa1e2f25c2129571c376f13f40b809883
mdmasharafilhossain <mashrafilmahi007@gmail.com>
2025-07-11 18:03:15 +0600

    AppError Class Updated

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -7,10 +7,13 @@ import AppError from "../errorHelpers/AppError";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
  let statusCode = 500
- let message = `Something Went Wrong!! ${err.message}`
+ let message = `Something Went Wrong!!`
 
 if(err instanceof AppError){
     statusCode = err.statusCode
+    message = err.message
+}else if( err instanceof Error){
+    statusCode = 500;
     message = err.message
 }
 
